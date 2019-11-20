f2932aa0b97d958b23fc1eb3b4fda45554094063
Brian Withrow <bdwithrow2115@eagle.fgcu.edu>
2019-11-20 12:11:21 -0500

    Title Scene and Menus
    Added Menus that can switch between a title scene and the lab scene, as well as quit the application. Log-In sets user and pass values but does nothing with them atm.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    private string username;
+    private string password;
+    public GameObject usernameField;
+    public GameObject passwordField;
+
+    public void checkSignIn()
+    {
+        username = usernameField.GetComponent<Text>().text;
+        password = passwordField.GetComponent<Text>().text;
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void toMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
