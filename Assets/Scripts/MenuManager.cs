c999d509045d34234ef76e8a75d7753b0fba14ef
Brian Withrow <bdwithrow2115@eagle.fgcu.edu>
2019-12-02 13:54:51 -0500

    UI Stuff being pushed

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -11,16 +11,42 @@ public class MenuManager : MonoBehaviour
     private string password;
     public GameObject usernameField;
     public GameObject passwordField;
+    public bool guest;
+
+    public static MenuManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        
+
+        DontDestroyOnLoad(gameObject);
+    }
 
     public void checkSignIn()
     {
         username = usernameField.GetComponent<Text>().text;
         password = passwordField.GetComponent<Text>().text;
-
     }
 
-    public void PlayGame()
+    public void PlayGameGuest()
     {
+        guest = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayGameUser()
+    {
+        Debug.Log("This worked");
+        guest = true;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
@@ -32,5 +58,18 @@ public class MenuManager : MonoBehaviour
     public void toMainMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void loadOptions()
+    {
+        /*if (guest)
+        {
+            foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("UserOptions"))
+            {
+                gameObject.active = false;
+            }
+            
+        }
+        */
     }
 }
